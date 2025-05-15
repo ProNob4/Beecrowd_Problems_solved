a6b65cd8d57bcc0ec02b6f0a7a086d022356ae87
ProNob <112222527+ProNob4@users.noreply.github.com>
2025-05-16 05:53:45 +0600

    Create 1113 - Ascending and Descending.c

---
+++ 1113 - Ascending and Descending.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    while(1)
+    {
+    int x,y;
+    scanf("%d%d",&x,&y);
+    
+    if(x<y)
+        printf("Crescente\n");
+        
+    else if(x>y)
+            printf("Decrescente\n");
+    else
+        break;
+    }
+    return 0;
+}
