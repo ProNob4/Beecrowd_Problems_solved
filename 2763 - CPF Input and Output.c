27ca08e5688889e6dd57be9a6fa6720a1ff57e2a
ProNob <112222527+ProNob4@users.noreply.github.com>
2025-05-16 19:26:21 +0600

    Create  2763 - CPF Input and Output.c

---
+++ 2763 - CPF Input and Output.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int x,y,z,d;
+    scanf("%d.%d.%d-%d",&x,&y,&z,&d);
+    printf("%.3d\n",x);
+    printf("%.3d\n",y);
+    printf("%.3d\n",z);
+    printf("%.2d\n",d);
+    return 0;
+}
